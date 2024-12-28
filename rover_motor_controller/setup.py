a3afc7d4f98684a00be938ac55e67d486a5461a4
Miguel Ángel González Santamarta <mgons@unileon.es>
2024-12-28 14:49:35 +0100

    cleaning cmakelist and package files

--- rover_motor_controller/setup.py
+++ rover_motor_controller/setup.py
@@ -2,29 +2,28 @@ from glob import glob
 import os
 from setuptools import setup, find_packages
 
-package_name = 'rover_motor_controller'
+package_name = "rover_motor_controller"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share/', package_name, 'launch/'), glob('launch/*.launch.py')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share/", package_name, "launch/"), glob("launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Miguel Ángel González Santamarta',
-    maintainer_email='mgons@unileon.es',
-    description='Rover motor controller package (Python)',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Miguel Ángel González Santamarta",
+    maintainer_email="mgons@unileon.es",
+    description="Rover motor controller package (Python)",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'controller_node = rover_motor_controller.controller_node_main:main',
-            'vel_parser_node = rover_motor_controller.vel_parser_node_main:main',
+        "console_scripts": [
+            "controller_node = rover_motor_controller.controller_node_main:main",
+            "vel_parser_node = rover_motor_controller.vel_parser_node_main:main",
         ],
     },
 )
--- rover_teleop/setup.py
+++ rover_teleop/setup.py
@@ -2,29 +2,28 @@ from glob import glob
 import os
 from setuptools import setup, find_packages
 
-package_name = 'rover_teleop'
+package_name = "rover_teleop"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share/', package_name, 'launch/'), glob('launch/*.launch.py')),
-        (os.path.join('share/', package_name, 'config/'), glob('config/*.yaml'))
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share/", package_name, "launch/"), glob("launch/*.launch.py")),
+        (os.path.join("share/", package_name, "config/"), glob("config/*.yaml")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Miguel Ángel González Santamarta',
-    maintainer_email='mgons@unileon.es',
-    description='Rover teleop package',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Miguel Ángel González Santamarta",
+    maintainer_email="mgons@unileon.es",
+    description="Rover teleop package",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'teleop_keyboard_node = rover_teleop.teleop_keyboard_node:main',
+        "console_scripts": [
+            "teleop_keyboard_node = rover_teleop.teleop_keyboard_node:main",
         ],
     },
 )
